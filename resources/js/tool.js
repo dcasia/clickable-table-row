a48af7d7767cc4b5fcf7ba4fc1c3f6348c93979a
milewski <rafael.milewski@gmail.com>
2020-05-02 10:38:04 +0800

    fix code style

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,48 +1,63 @@
 Nova.booting((Vue, router, store) => {
-  const listeners = [];
+
+  const listeners = []
 
   function navigateToResource(event) {
-    const path = event.path || (event.composedPath && event.composedPath());
 
-    const intersectsWithIgnoredElements = path.some(
-      (path) =>
+    const path = event.path || ( event.composedPath && event.composedPath() )
+
+    const intersectsWithIgnoredElements = path.some(path =>
         path instanceof HTMLAnchorElement ||
         path instanceof HTMLInputElement ||
         path instanceof HTMLButtonElement ||
-        path instanceof SVGElement
-    );
+        path instanceof SVGElement)
 
     /**
      * Avoid following click when clicking on A tags or when selecting text
      */
-    if (!intersectsWithIgnoredElements && window.getSelection().toString() === "") {
-      let selector = 'a[dusk$="-' + (event.altKey ? "edit" : "view") + '-button"]';
-      const viewElement = this.querySelector(selector);
+    if (!intersectsWithIgnoredElements && window.getSelection().toString() === '') {
+
+      const selector = 'a[dusk$="-' + ( event.altKey ? 'edit' : 'view' ) + '-button"]'
+      const viewElement = this.querySelector(selector)
 
       if (viewElement) {
-        viewElement.click();
+
+        viewElement.click()
+
       }
+
     }
+
   }
 
-  Nova.$on("resources-loaded", () => {
+  Nova.$on('resources-loaded', () => {
+
     while (listeners.length) {
-      listeners.pop().removeEventListener("click", navigateToResource);
+
+      listeners.pop().removeEventListener('click', navigateToResource)
+
     }
 
     Vue.nextTick(() => {
-      const viewElement = document.querySelector('a[dusk$="-view-button"]');
+
+      const viewElement = document.querySelector('a[dusk$="-view-button"]')
 
       if (viewElement) {
-        const rows = document.querySelectorAll('table[data-testid="resource-table"] tr[dusk$="-row"]');
+
+        const rows = document.querySelectorAll('table[data-testid="resource-table"] tr[dusk$="-row"]')
 
         for (const row of rows) {
-          row.style.cursor = "pointer";
-          row.addEventListener("click", navigateToResource);
 
-          listeners.push(row);
+          row.style.cursor = 'pointer'
+          row.addEventListener('click', navigateToResource)
+
+          listeners.push(row)
+
         }
       }
-    });
-  });
-});
+
+    })
+
+  })
+
+})
