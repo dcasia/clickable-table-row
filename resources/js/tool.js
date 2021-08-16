b5c290ed4259c87393b7734016df8c52ae9147c4
Rafael Milewski <rafael.milewski@gmail.com>
2021-08-16 21:54:23 +0800

    Fix issue with select dropdowns within table rows (#27)

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,63 +1,64 @@
 Nova.booting((Vue, router, store) => {
 
-  const listeners = []
+    const listeners = []
 
-  function navigateToResource(event) {
+    function navigateToResource(event) {
 
-    const path = event.path || ( event.composedPath && event.composedPath() )
+        const path = event.path || (event.composedPath && event.composedPath())
 
-    const intersectsWithIgnoredElements = path.some(path =>
-        path instanceof HTMLAnchorElement ||
-        path instanceof HTMLInputElement ||
-        path instanceof HTMLButtonElement ||
-        path instanceof SVGElement)
+        const intersectsWithIgnoredElements = path.some(path =>
+            path instanceof HTMLAnchorElement ||
+            path instanceof HTMLInputElement ||
+            path instanceof HTMLButtonElement ||
+            path instanceof SVGElement ||
+            path instanceof HTMLSelectElement)
 
-    /**
-     * Avoid following click when clicking on A tags or when selecting text
-     */
-    if (!intersectsWithIgnoredElements && window.getSelection().toString() === '') {
+        /**
+         * Avoid following click when clicking on A tags or when selecting text
+         */
+        if (!intersectsWithIgnoredElements && window.getSelection().toString() === '') {
 
-      const selector = 'a[dusk$="-' + ( event.altKey ? 'edit' : 'view' ) + '-button"]'
-      const viewElement = this.querySelector(selector)
+            const selector = 'a[dusk$="-' + (event.altKey ? 'edit' : 'view') + '-button"]'
+            const viewElement = this.querySelector(selector)
 
-      if (viewElement) {
+            if (viewElement) {
 
-        viewElement.click()
+                viewElement.click()
 
-      }
-
-    }
-
-  }
-
-  Nova.$on('resources-loaded', () => {
-
-    while (listeners.length) {
-
-      listeners.pop().removeEventListener('click', navigateToResource)
-
-    }
-
-    Vue.nextTick(() => {
-
-      const viewElement = document.querySelector('a[dusk$="-view-button"]')
-
-      if (viewElement) {
-
-        const rows = document.querySelectorAll('table[data-testid="resource-table"] tr[dusk$="-row"]')
-
-        for (const row of rows) {
-
-          row.style.cursor = 'pointer'
-          row.addEventListener('click', navigateToResource)
-
-          listeners.push(row)
+            }
 
         }
-      }
+
+    }
+
+    Nova.$on('resources-loaded', () => {
+
+        while (listeners.length) {
+
+            listeners.pop().removeEventListener('click', navigateToResource)
+
+        }
+
+        Vue.nextTick(() => {
+
+            const viewElement = document.querySelector('a[dusk$="-view-button"]')
+
+            if (viewElement) {
+
+                const rows = document.querySelectorAll('table[data-testid="resource-table"] tr[dusk$="-row"]')
+
+                for (const row of rows) {
+
+                    row.style.cursor = 'pointer'
+                    row.addEventListener('click', navigateToResource)
+
+                    listeners.push(row)
+
+                }
+            }
+
+        })
 
     })
-
-  })
 
 })
